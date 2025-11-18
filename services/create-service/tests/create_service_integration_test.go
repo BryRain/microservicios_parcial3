321afcaa993e19e94d02d8e17da56048ba36ab7b
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-17 22:31:19 -0500

    implementacion

--- services/create-service/tests/create_service_integration_test.go
+++ services/create-service/tests/create_service_integration_test.go
@@ -1,26 +1,25 @@
 package tests
-}
-	}
-		t.Fatalf("Ping falló: %v", err)
-	if err := client.Ping(ctx, nil); err != nil {
-	}
-		t.Fatalf("No se pudo conectar a MongoDB: %v", err)
-	if err != nil {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	defer cancel()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		"@" + os.Getenv("MONGO_HOST") + ":" + os.Getenv("MONGO_PORT")
-	uri := "mongodb://" + os.Getenv("MONGO_USER") + ":" + os.Getenv("MONGO_PASS") +
-func TestMongoConnection(t *testing.T) {
 
-)
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo"
-
-	"time"
-	"testing"
-	"os"
-	"context"
 import (
+	"context"
+	"os"
+	"testing"
+	"time"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
+func TestMongoConnection(t *testing.T) {
+	uri := "mongodb://" + os.Getenv("MONGO_USER") + ":" + os.Getenv("MONGO_PASSWORD") +
+		"@" + os.Getenv("MONGO_HOST") + ":" + os.Getenv("MONGO_PORT")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		t.Fatalf("No se pudo conectar a MongoDB: %v", err)
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		t.Fatalf("Ping falló: %v", err)
+	}
+}
